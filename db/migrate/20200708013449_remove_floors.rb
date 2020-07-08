a91e9e6af57dd4c616767f96a0b61aa27abe6a83
“irobetson” <ianrobertson427@gmail.com>
2020-07-07 21:12:12 -0500

    testing lat/lon

--- db/migrate/20200708013449_remove_floors.rb
+++ db/migrate/20200708013449_remove_floors.rb
@@ -1,4 +1,7 @@
 class RemoveFloors < ActiveRecord::Migration[6.0]
   def change
+    remove_column :houses, :floors, :integer
+    add_column :houses, :latitude, :decimal, { precision: 10, scale: 6 }
+    add_column :houses, :longitude, :decimal, { precision: 10, scale: 6 }
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_08_012549) do
+ActiveRecord::Schema.define(version: 2020_07_08_013449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 2020_07_08_012549) do
     t.integer "square_feet"
     t.integer "bedrooms"
     t.integer "bathrooms"
-    t.integer "floors"
     t.string "availabilty"
     t.integer "price"
     t.string "address"
